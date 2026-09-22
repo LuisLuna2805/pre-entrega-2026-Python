55f4dbbe1b0f0372e2c9168e4848db6e34313de7
LuchiLuna <luiluna@frba.utn.edu.ar>
2026-09-22 10:36:09 -0300

    version 2.0

--- main.py
+++ main.py
@@ -1,42 +1,87 @@
-productos = [ "pera" , "manzana" , "kiwi"]
+#Menu con 5 opciones: Listar, Remover, Agregar, Salir y Buscar
+productos = ["Cerveza", "Pepsi", "Coca", "Fanta"]
+contador_de_errores = 0
 
 while True:
-    fruta_nueva = input("Ingrese nueva fruta o SALIR: ")
-    if fruta_nueva.upper() == "SALIR":
-        break
-    else:
-        productos.append(fruta_nueva)
-
-while True:
-    print("\nMENU: "
-        "\n'L' - listar productos" \
-        "\n'A' - agregar producto" \
-        "\n'X' - remover productos por nombre" \
-        "\n'R' - remover productos por posicion" \
-        "\n'P' - ver un producto en particular " \
-        "\n'B' - Buscar un producto")
-
-    tecla = input("\nIngrese una Tecla: ")
+    opciones = input("-----------------------------\n"
+                    "**** Menu de opciones **** \n\n"
+                    "       'L' - Listar\n"
+                    "       'R' - Remover por nombre\n"
+                    "       'X' - Remover por indice\n"
+                    "       'A' - Agregar\n"
+                    "       'B' - Buscar\n"
+                    "       'S' - Salir\n"
+                    "-----------------------------\n"
+                    "Ingrese una letra: ")
     
-    if tecla.upper() == "L":
-        print(productos)
-    elif tecla.upper() == "A":
-        fruta = input("Ingrese fruta a agregar: ")
-        productos.append(fruta)
-    elif tecla.upper() == "X":
-        elemento = (input("Ingrese una elemento a quitar: "))
-        productos.remove(elemento)      
-    elif tecla.upper() == "R":
-        posicion = int(input("Ingrese una posicion: "))
-        productos.pop(posicion)
-    elif tecla.upper() == "P":
-        posicion = int(input("Ingrese una posicion: "))
-        print(f"producto en posicion {posicion} es {productos[posicion]}")
-    elif tecla.upper() == "B":
-        buscar = input("Ingrese fruta a buscar: ")
-        if buscar in productos:
-            print(f"{buscar} esta dentro de la lista")
+    #Comienza menu de opciones
+    #Opcion para Listar
+    if opciones.strip().upper() == "L":
+            contador_de_errores = 0
+            print("\nListado de productos:")
+            for i, producto in enumerate(productos):
+                print(f"{i}.{producto}")
+
+    #Opcion para Remover un producto por nombre
+    elif opciones.strip().upper() == "R":
+        contador_de_errores = 0
+        producto_removido = input("Ingrese producto a remover:\n")
+        producto_removido_limpio = producto_removido.strip().capitalize()
+        if producto_removido_limpio in productos:
+            productos.remove(producto_removido_limpio)
+            print(f"{producto_removido_limpio} removido con exito.\n")
         else:
-            print(f"{buscar} no esta dentro de la lista")
-    else:
+            print(f"No se pudo remover {producto_removido_limpio} porque no pertenecia a la lista.\n")
+
+    # #Opcion para Remover un producto por posicion
+    elif opciones.strip().upper() == "X":
+        contador_de_errores = 0
+        entrada = input("Ingrese indice a remover:\n").strip()
+        if not entrada.isdigit():
+            print("Por favor ingrese un número válido.\n")
+        else:
+            indice_removido = int(entrada)
+            if indice_removido < len(productos):
+                producto_removido_limpio = productos[indice_removido]
+                productos.pop(indice_removido)
+                print(f"producto '{indice_removido}.{producto_removido_limpio}' removido con exito.\n")
+            else:
+                print(f"No se pudo remover '{indice_removido}' porque no pertenecia a la lista.\n")    
+
+    #Opcion para Agregar un producto
+    elif opciones.strip().upper() == "A":
+        contador_de_errores = 0
+        producto_agregado = input("Ingrese producto a agregar:\n")
+        producto_agregado_limpio = producto_agregado.strip().capitalize()
+        if producto_agregado_limpio in productos:
+            print(f"El producto {producto_agregado_limpio} ya pertenece a la lista.\n")
+        else:
+            productos.append(producto_agregado_limpio)
+            print(f"{producto_agregado_limpio} agregado con exito.\n")
+
+    #Opcion para Buscar un producto
+    elif opciones.strip().upper() == "B":
+        contador_de_errores = 0
+        producto_buscado = input("Ingrese producto a buscar:\n")
+        producto_buscado_limpio = producto_buscado.strip().capitalize()
+        if producto_buscado_limpio in productos:
+            print(f"Producto encontrado, hay {producto_buscado_limpio}.\n")
+        else:
+            print(f"Perdon, no tenemos {producto_buscado_limpio}.\n")
+
+    #Opcion para Salir
+    elif opciones.strip().upper() == "S":
+        print("Saliendo...")
         break
+
+    #Opcion por si no ingresa una opcion dentro del menu
+    else:
+        print("Por favor ingrese alguna de la opciones dentro del menu.\n")
+        contador_de_errores += 1
+        if contador_de_errores > 5:
+            print(f"contador de errores: {contador_de_errores}")
+            print("Si ingresa varios errores seguidos el programa se cerrara.\n")
+            if contador_de_errores == 10:
+                print("Saliendo...")
+                break
+print(f"Gracias por usar nuestra aplicacion.")
